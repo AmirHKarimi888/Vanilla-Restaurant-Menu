af452d5a26b70454f4fe7ecb6e9bcfdea738d5ad
AmirHK888 <amirhkarimi888@gmail.com>
2023-11-08 18:56:52 +0330

    Blank project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./main.js",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
